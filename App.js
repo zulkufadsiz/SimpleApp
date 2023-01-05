3d9c802c206a4684ef77ff75f9dd1f44a25c793b
zulkufadsiz <zulkufadsiz@gmail.com>
2023-01-05 18:32:03 +0100

    Only dead people can see

--- App.js
+++ App.js
@@ -75,7 +75,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Only dead people can see">
+          <Section title="Only dead people can see.">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
